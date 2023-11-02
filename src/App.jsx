28aa7b66745d9df0f5de30fd5ce12789a6fb32c8
Hossein <81298314+hosseinghafouri@users.noreply.github.com>
2023-11-02 09:27:03 -0700

    query

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './pages/Header.jsx';
@@ -8,12 +8,19 @@ import About from './pages/content/About.jsx';
 import Erorr from './pages/Erorr.jsx';
 import Footer from './pages/Footer.jsx';
 import { useState, createContext } from 'react';
+import { QueryClient, QueryClientProvider } from 'react-query';
 export const profileContext = createContext();
+
+
 function App() {
+  const client = new QueryClient({defaultOptions :{
+    queries :{refetchOnWindowFocus: false},
+  }});
   const [username, setUsername] = useState("Hossein");
   return (
     <profileContext.Provider value={{username, setUsername}}>
     <div className="App">
+      <QueryClientProvider client={client}>
      <Router>
       <Header />
       <Routes>
@@ -24,6 +31,7 @@ function App() {
       </Routes>
       <Footer />
      </Router>
+     </QueryClientProvider>
     </div>
     </profileContext.Provider>
   );
--- src/pages/content/Home.jsx
+++ src/pages/content/Home.jsx
@@ -1,12 +1,23 @@
 import { useContext } from "react";
+import { useQuery } from "react-query";
+import Axios from 'axios'
 import { profileContext } from "../../App";
 function Home(props) {
-    const {username} = useContext(profileContext);
-    return (
-        <div className="Home">
-            
-            <h1>Home - username: {username}</h1>
-        </div>
-    )
+  const {data :catData, isLoading, isError, error, refetch} = useQuery(["cat"], () => {
+    return Axios.get(`https://catfact.ninja/fact`).then((res) => res.data)
+  });
+
+  const { username } = useContext(profileContext);
+  if (isLoading) return <h2>is loading  ...</h2>;
+  if (isError) return <h2>is error  ... {error.message}</h2>;
+
+  return (
+    
+    <div className="Home">
+      <h1>Home - username: {username}</h1>
+      <p>{catData?.fact}</p>
+      <button onClick={refetch}>Update</button>
+    </div>
+  );
 }
 export default Home;
